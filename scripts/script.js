596feff3d1d94cb0d55fa1230013c0308b5edbb2
Yann Leyers <leyersyann@gmail.com>
2024-03-21 10:28:07 +0100

    second version

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,30 @@ const canvas = document.getElementById('myCanvas');
     const ctx = canvas.getContext('2d');
     const width = canvas.width = window.innerWidth;
     const height = canvas.height = window.innerHeight;
-    
+    const instructionText = document.getElementById('instructionText');
+
     let isDrawing = false;
+    let strokeColor = '#000000'; // Default color
+    let lineSize = 1; // Default line size
+    
+    // Color picker event listener
+    const colorPicker = document.getElementById('colorPicker');
+    colorPicker.addEventListener('input', function() {
+        strokeColor = this.value;
+    });
+
+    // Line size event listener
+    const lineSizeInput = document.getElementById('lineSizeInput');
+    lineSizeInput.addEventListener('input', function() {
+        lineSize = parseInt(this.value);
+    });
+
+    // Reset button event listener
+    const resetButton = document.getElementById('resetButton');
+    resetButton.addEventListener('click', function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        instructionText.style.display = 'block'; // Display instruction text
+    });
     
     canvas.addEventListener('mousedown', function(event) {
         isDrawing = true;
@@ -11,6 +33,9 @@ const canvas = document.getElementById('myCanvas');
         const mouseY = event.clientY - canvas.getBoundingClientRect().top;
         ctx.beginPath();
         ctx.moveTo(mouseX, mouseY);
+        ctx.strokeStyle = strokeColor;
+        ctx.lineWidth = lineSize;
+        instructionText.style.display = 'none'; // Hide instruction text
     });
     
     canvas.addEventListener('mousemove', function(event) {
@@ -38,6 +63,9 @@ const canvas = document.getElementById('myCanvas');
         const mouseY = touch.clientY - canvas.getBoundingClientRect().top;
         ctx.beginPath();
         ctx.moveTo(mouseX, mouseY);
+        ctx.strokeStyle = strokeColor;
+        ctx.lineWidth = lineSize;
+        instructionText.style.display = 'none'; // Hide instruction text
     });
     
     canvas.addEventListener('touchmove', function(event) {
